d7544e0659c6e8da13200f864120cd868b4e9ac5
Pedro santos <pedrosantosjovem2004@gmail.com>
2024-02-22 17:13:26 -0300

    novas

---
+++ pass_por_valor.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int subtrair(int x, int y){
+    printf("posiçao x %p e posição y %p\n",&x,&y);
+    return x - y;
+}
+
+int main(){
+    int v1 = 10;
+    int v2 = 5;
+    int rs = subtrair(v1,v2);
+    printf("posiçao de v1 %p e posição de v2 %p\n",&v1,&v2);
+    printf("o resultado e %d\n",rs);
+    return 0;
+}
+
